b22cbb6ac2286d67887ee04920b17ba3022c22ee
Breno Garcia <BrenoGHeat@gmail.com>
2024-12-20 12:37:07 -0300

    aulas de relacionamentos / relacionamentos criados Django

---
+++ albums/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+
+    musician = models.ForeignKey(
+        "musicians.Musician",
+        on_delete=models.CASCADE,
+        related_name="albums",
+    )
